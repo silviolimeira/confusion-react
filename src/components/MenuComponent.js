f6bad03990b7a1f32b7030a514235d0e6d5be595
Silvio Cesar Marqui Limeira <silvio.limeira@live.com>
2020-05-12 14:33:12 -0300

    Components Part 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -7,6 +7,29 @@ class Menu extends Component {
         super(props);
 
         this.state = {
+            selectDish: null
+        }
+    }
+
+    onDishSelect(dish) {
+        this.setState({ selectDish: dish});
+    }
+
+    renderDish(dish) {
+        if (dish != null) {
+            return (
+                <Card>
+                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            )
+        } else {
+            return (
+                <div></div>
+            );
         }
     }
 
@@ -15,7 +38,7 @@ class Menu extends Component {
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className='col-12 col-md-5 m-1'>
-                    <Card>
+                    <Card onClick={() => this.onDishSelect(dish)}>
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -29,6 +52,9 @@ class Menu extends Component {
             <div className="container">
                 <div className="row">
                     {menu}
+                </div>
+                <div className="row">
+                    {this.renderDish(this.state.selectDish)}
                 </div>
             </div>
 
