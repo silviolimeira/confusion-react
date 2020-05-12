084d618b34b804c9646f6c47e50489797a518e8e
Silvio Cesar Marqui Limeira <silvio.limeira@live.com>
2020-05-12 09:23:14 -0300

    add assets images

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -15,7 +15,7 @@ class Menu extends Component {
                     category: 'mains',
                     label: 'Hot',
                     price: '4.99',
-                    description: 'A unique combination of Indian'
+                    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
                 },
                 {
                     id: 1,
@@ -24,7 +24,7 @@ class Menu extends Component {
                     category: 'appetizer',
                     label: '',
                     price: '1.99',
-                    description: 'Deep fried Zucchini coated'
+                    description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
                 },
                 {
                     id: 2,
@@ -33,7 +33,7 @@ class Menu extends Component {
                     category: 'appetizer',
                     label: 'New',
                     price: '1.99',
-                    description: 'A quintessential Confusion'
+                    description: 'A quintessential ConFusion experience, is it a vada or is it a donut?'
                 },
                 {
                     id: 3,
@@ -42,9 +42,8 @@ class Menu extends Component {
                     category: 'dessert',
                     label: '',
                     price: '2.99',
-                    description: 'A delectable, semi-sweet New'
+                    description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
                 }
-
             ]
         }
     }
